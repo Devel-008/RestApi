4384a8fd4dec16f68048ff1d660be241eb1a4315
ISHA SETHIA <isha.sethia@confluxsys.com>
2022-10-21 15:25:12 +0530

    next commit

--- src/main/java/demo/rest/api/practice/UserService.java
+++ src/main/java/demo/rest/api/practice/UserService.java
@@ -15,26 +15,6 @@ public class UserService {
     final Logger logger = LoggerFactory.getLogger(UserService.class);
     ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
     UserRepository repo = context.getBean("userDao",UserRepository.class);
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("use")
-    public List<User> getUsers(){
-        logger.info("Logger Intialized to getUsers");
-        /*User user = new User(2,"jina","hiiiii","1999-10-10");
-        Response response = Response.status(200).entity(user).build();*/
-        return repo.getUserList();
-    }
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("user/{id}")
-    public User getUser(@PathParam("id") int id){
-        User user = repo.getUser(id);
-      //  Response response = Response.status(200).entity(user).build();
-       // System.out.println(repo.getUser(id));
-        repo.tearDown();
-        return user;
-
-    }
     //adduser(User user)
     @POST
     @Path("/create")
@@ -44,27 +24,5 @@ public class UserService {
         repo.create(u1,logger);
         return u1;
     }
-    @PUT
-    @Path("/update")
-    @Produces(MediaType.APPLICATION_JSON)
-    public User update(User u1){
-        System.out.println(u1);
-        if(repo.getUser(u1.getId()).getId()==0){
-            repo.create(u1,logger);
-        }else {
-            repo.update(u1);
-        }
-        return u1;
-    }
-    @DELETE
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("user/{id}")
-    public User delete(@PathParam("id") int id){
-        User user = repo.getUser(id);
-        //  Response response = Response.status(200).entity(user).build();
-        // System.out.println(repo.getUser(id));
-        if(user.getId() != 0)
-        repo.delete(id);
-        return user;
-    }
+
 }
